f596027e584b256eea8b0ded4372f2535f791f21
ShokoMatsuda <syokom.0128@gmail.com>
2023-10-29 17:42:49 +0900

    初回コミット

---
+++ main.js
@@ -0,0 +1,14 @@
+// ランダムなテキストを表示
+const creatText = () => {};
+
+// キー入力の判定
+const keyPress = e => {};
+
+// タイピングスキルのランクを判定
+const rankCheack = score => {};
+
+// ゲームを終了
+const gameOver = id => {};
+
+// カウントダウンタイマー
+const timer = () => {};
