054d01b1bb87d6e70d50c2d1139fa8e8d711018f
ShokoMatsuda <syokom.0128@gmail.com>
2023-11-02 21:32:52 +0900

    ゲームの判定結果を表示しようまで実装

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 // 変数の初期化
 let untyped = '';
 let typed = '';
+let score = 0;
 
 // 必要なHTML要素の取得
 const untypedfield = document.getElementById('untyped');
@@ -59,7 +60,8 @@ const keyPress = e => {
     }
 
     // 正タイプの場合
-    wrap.classList.remove('mistyped');
+    // スコアのインクリメント
+    score++;
     typed += untyped.substring(0, 1);
     untyped = untyped.substring(1);
     typedfield.textContent = typed;
@@ -72,13 +74,37 @@ const keyPress = e => {
 };
 
 // タイピングスキルのランクを判定
-const rankCheck = score => {};
+const rankCheck = score => {
+
+    // テキストを格納する変数を作る
+    let text = '';
+
+    // スコアに応じて異なるメッセージを変数textに格納する
+    if(score < 100) {
+        text = `あなたのランクはCです。\nBランクまであと${100 - score}文字です。`;
+    }  else if(score < 200) {
+        text = `あなたのランクはBです。\nAランクまであと${200 - score}文字です。`;
+    } else if(score < 300) {
+        text = `あなたのランクはAです。\nSランクまであと${300 - score}文字です。`;
+    } else if(score >= 300) {
+        text = `あなたのランクはSです。\nおめでとうございます!`;
+    }
+
+    // 生成したメッセージと一緒に文字列を返す
+    return `${score}文字打てました！\n${text}\n【OK】リトライ / 【キャンセル】終了`;
+
+};
 
 // ゲームを終了
 const gameOver = id => {
     clearInterval(id);
 
-    console.log('ゲーム終了');
+    const result = confirm(rankCheck(score));
+
+    // OKボタンをクリックされたらリロードする
+    if(result == true) {
+        window.location.reload();
+    }
 };
 
 // カウントダウンタイマー
@@ -100,8 +126,6 @@ const timer = () => {
     }, 1000);
 };
 
-// キーボードのイベント処理
-document.addEventListener('keypress', keyPress);
 
 // ゲームスタート時の処理
 start.addEventListener('click', () => {
